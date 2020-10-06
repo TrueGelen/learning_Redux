970999f96d13b57ffc0794837c78752dda013a27
TrueGelen <54402826+TrueGelen@users.noreply.github.com>
2020-10-06 18:59:05 +0300

    set up

---
+++ README.md
@@ -0,0 +1,4 @@
+# [ParodyShop](https://truegelen.github.io/ReactAppStore/dist/)
+This is a training project. [Link to this page](https://truegelen.github.io/ReactAppStore/dist/)
+---------------------------------
+in a process
---
+++ src/index.js
@@ -0,0 +1,11 @@
+/* import '../node_modules/normalize.css/normalize.css'
+
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './js/app'
+ */
+import scss from './scss/main.module.scss'
+/* ReactDom.render(
+  <Provider rootStore={rootStore}><App></App></Provider>,
+  document.querySelector('#app')
+) */
